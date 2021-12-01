0d0310550e2f790c3e8d48456b5196f2eb5f2511
Scott Stroz <scott.stroz@gmail.com>
2021-12-01 10:58:33 -0500

    Completed part 2 of day 1

---
+++ Day1/part2.py
@@ -0,0 +1,23 @@
+import json
+import array
+
+f = open( 'data.json' )
+data = json.load( f )
+
+myCount = 0
+if len( data ) > 2:
+    prevSet = data[ 0 ] + data[ 1 ] + data[ 2 ];
+else:
+    prevSet = 0
+i = 0
+while i < len( data ) - 2:
+    thisSet = data [ i ] + data[ i+ 1 ] + data[ i + 2 ]
+    if thisSet > prevSet:
+        myCount += 1
+    prevSet = thisSet
+    i += 1
+
+
+print( myCount )
+
+f.close()
